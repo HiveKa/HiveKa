de7341fc925b72cd6c123927fa6f0b2acbd42704
asingh <asingh@cloudera.com>
2014-11-06 12:59:18 -0800

    Package yammer and scala

--- src/main/java/org/apache/hadoop/hive/kafka/KafkaInputFormat.java
+++ src/main/java/org/apache/hadoop/hive/kafka/KafkaInputFormat.java
@@ -107,6 +107,7 @@ public class KafkaInputFormat implements InputFormat<KafkaKey, CamusWrapper> {
 		List<TopicMetadata> topicMetadataList = null;
 		Exception savedException = null;
 		while (i < brokers.size() && !fetchMetaDataSucceeded) {
+      log.info("Trying to connect to broker: " + brokers.get(i));
 			SimpleConsumer consumer = createConsumer(conf, brokers.get(i));
 			log.info(String.format("Fetching metadata from broker %s with client id %s for %d topic(s) %s",
 			brokers.get(i), consumer.clientId(), metaRequestTopics.size(), metaRequestTopics));
